004405d762c3f9d6da562773f8e48718e0645b6b
engam <engamrdiab52@gmail.com>
2022-07-25 13:56:00 +0200

    INITIAL

---
+++ app/src/main/java/com/amrabdelhamiddiab/qrscanner_ml/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.amrabdelhamiddiab.qrscanner_ml
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Size
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.amrabdelhamiddiab.qrscanner_ml.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val REQUEST_CODE: Int = 1001
+        const val TAG = "MainActivity"
+    }
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var analyzer: MyImageAnalyzer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        askForCameraPermission()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener( {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+        analyzer = MyImageAnalyzer(this)
+    }
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder()
+            .build()
+        val cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        imageAnalysis.setAnalyzer(cameraExecutor, analyzer)
+
+        cameraProvider.bindToLifecycle(
+            this as LifecycleOwner,
+            cameraSelector,
+            imageAnalysis,
+            preview
+        )
+    }
+
+    private fun askForCameraPermission() {
+        if (askForPermissions()) {
+            // Permissions are already granted, do your stuff
+            Toast.makeText(this, "permission Already granted", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun askForPermissions(): Boolean {
+        if (!isPermissionsAllowed()) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this as Activity,
+                    Manifest.permission.CAMERA
+                )
+            ) {
+                showPermissionDeniedDialog()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this as Activity,
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_CODE
+                )
+            }
+            return false
+        }
+        return true
+    }
+
+    private fun isPermissionsAllowed(): Boolean {
+
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission is granted, you can perform your operation here
+                    Toast.makeText(this, "permission granted",Toast.LENGTH_SHORT).show()
+
+                } else {
+                    // permission is denied, you can ask for permission again, if you want
+                    //  askForPermissions()
+                    Toast.makeText(this, "permission denied",Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Permission Denied")
+            .setMessage("Permission is denied, Please allow permissions from App Settings.")
+            .setPositiveButton("App Settings"
+            ) { _, _ ->
+                // send to app settings if permission is denied permanently
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+}
+
+
---
+++ app/src/main/java/com/amrabdelhamiddiab/qrscanner_ml/MyImageAnalyzer.kt
@@ -0,0 +1,47 @@
+package com.amrabdelhamiddiab.qrscanner_ml
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.amrabdelhamiddiab.qrscanner_ml.MainActivity.Companion.TAG
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+
+class MyImageAnalyzer(private val context: Context) : ImageAnalysis.Analyzer {
+    override fun analyze(imageProxy: ImageProxy) {
+        scanBarcode(imageProxy)
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError", "UnsafeOptInUsageError")
+    private fun scanBarcode(imageProxy: ImageProxy) {
+        imageProxy.image?.let { image ->
+            val inputImage = InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
+            val scanner = BarcodeScanning.getClient()
+            scanner.process(inputImage)
+                .addOnCompleteListener {
+                    imageProxy.close()
+                    if (it.isSuccessful) {
+                        readBarcodeData(it.result as List<Barcode>)
+                    } else {
+                        it.exception?.printStackTrace()
+                    }
+                }
+        }
+    }
+
+    private fun readBarcodeData(barcodes: List<Barcode>) {
+        for (barcode in barcodes) {
+            when (barcode.valueType) {
+                Barcode.TYPE_TEXT -> {
+                      Toast.makeText(context, barcode.displayValue, Toast.LENGTH_SHORT).show()
+
+                    barcode.displayValue?.let { Log.d(TAG, it) }
+                }
+            }
+        }
+    }
+}
